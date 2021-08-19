3aeba1121cab75902c4ea1de54d261176c818956
Wilson Shirado <wilson.shirado@animaeducacao.com.br>
2021-08-19 17:24:02 -0300

    refactor: correções para deixar refactor para aula de TDD

--- src/models/Desempenho.java
+++ src/models/Desempenho.java
@@ -1,24 +1,7 @@
 package models;
 
 public enum Desempenho {
-    A_DESEJAR {
-        @Override
-        public double percentualReajuste() {
-            return 0.03;
-        }
-    },
-    BOM {
-        @Override
-        public double percentualReajuste() {
-            return 0.15;
-        }
-    },
-    OTIMO {
-        @Override
-        public double percentualReajuste() {
-            return 0.20;
-        }
-    };
-
-    public abstract double percentualReajuste();
+    A_DESEJAR ,
+    BOM,
+    OTIMO;    
 }
--- src/services/ReajusteService.java
+++ src/services/ReajusteService.java
@@ -5,7 +5,21 @@ import models.Funcionario;
 
 public class ReajusteService {
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        double reajuste = desempenho.percentualReajuste();
+        double reajuste;
+        switch(desempenho){
+            case A_DESEJAR:
+                reajuste = 0.03;
+                break;
+            case BOM:
+                reajuste = 0.15;
+                break;
+            case OTIMO:
+                reajuste = 0.20;
+                break;
+            default:
+                reajuste = 0;
+                break;
+        }        
         funcionario.reajustarSalario(reajuste);
     }
 }
