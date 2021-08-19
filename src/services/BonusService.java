4b696716c3861cca2cad26eceff928f4a9644b7b
hissamu.shirado <hissamu.shirado@gmail.com>
2021-08-19 15:10:45 -0300

    first commit

---
+++ src/services/BonusService.java
@@ -0,0 +1,18 @@
+package services;
+
+import java.math.BigDecimal;
+
+import models.Funcionario;
+import models.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			valor = BigDecimal.ZERO;
+		}
+		return valor;
+	}
+
+}
